a87469a422493adefb38340445bcb6ea4fc74324
Andrey5440 <128066760+Andrey5440@users.noreply.github.com>
2023-06-28 22:04:02 -0600

    Add files via upload

---
+++ view/menu.php
@@ -0,0 +1,22 @@
+<ul class="nav nav-tabs" id="navId" role="tablist">
+<li class="nav-item">
+  <a href="#tab1Id" class="nav-link active" data-bs-toggle="tab" aria-current="page">Actividad</a>
+</li>
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Cursos</a>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="<?php echo constant('URL');?>cursos">Consulta</a>
+    <a class="dropdown-item" href="<?php echo constant('URL');?>cursos/crear">Crear</a>
+
+  </div>
+</li>
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Grupos</a>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="<?php echo constant('URL');?>grupos">Consulta</a>
+    <a class="dropdown-item" href="<?php echo constant('URL');?>grupos/crear">Crear</a>
+
+  </div>
+</li>
+
+</ul>
